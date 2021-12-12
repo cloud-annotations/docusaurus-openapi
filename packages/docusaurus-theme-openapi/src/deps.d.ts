bf817bbc02d70e95739ebcb35c2c1ad60539fed2
Joshua Chen <sidachen2003@gmail.com>
2021-12-13 07:13:38 +0800

    Fix types

---
+++ packages/docusaurus-theme-openapi/src/deps.d.ts
@@ -0,0 +1,9 @@
+/* ============================================================================
+ * Copyright (c) Cloud Annotations
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
+// TODO: remove in the next Docusaurus upgrade
+declare module "@docusaurus/theme-translations";
