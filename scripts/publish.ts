7aed95c2ce351d99c751ddc47cc8e1b07331c872
Nick Bourdakos <bourdakos1@gmail.com>
2021-12-21 18:49:25 -0500

    Log versions

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -112,6 +112,7 @@ function versions() {
 }
 
 function main() {
+  console.log(versions());
   if (versions().includes(`v${version}`)) {
     console.log(yellow(`SKIPPING: Version ${version} already exists.`));
     return;
