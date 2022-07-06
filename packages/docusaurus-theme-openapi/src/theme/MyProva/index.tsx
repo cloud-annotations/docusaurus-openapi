477ef5071bac07034f8060f98804637b6ada56e0
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-06 14:41:40 +0200

    fix layout

--- packages/docusaurus-theme-openapi/src/theme/MyProva/index.tsx
+++ packages/docusaurus-theme-openapi/src/theme/MyProva/index.tsx
@@ -14,30 +14,10 @@ export default function MyProva(props: any) {
     frontMatter;
 
   return (
-    <>
-      Ciao: <pre>{JSON.stringify(props, null, 2)}</pre>
-      <pre>{Object.keys(props).join("\n")}</pre>
-      <pre>{Object.keys(MDXPageContent).join("\n")}</pre>
-      <main className="container container--fluid margin-vert--lg">
-        <div className={clsx("row", styles.mdxPageWrapper)}>
-          <div className={clsx("col", !hideTableOfContents && "col--8")}>
-            <article>
-              <MDXContent>
-                <MDXPageContent />
-              </MDXContent>
-            </article>
-          </div>
-          {!hideTableOfContents && MDXPageContent.toc.length > 0 && (
-            <div className="col col--2">
-              <TOC
-                toc={MDXPageContent.toc}
-                minHeadingLevel={frontMatter.toc_min_heading_level}
-                maxHeadingLevel={frontMatter.toc_max_heading_level}
-              />
-            </div>
-          )}
-        </div>
-      </main>
-    </>
+    <article>
+      <MDXContent>
+        <MDXPageContent />
+      </MDXContent>
+    </article>
   );
 }
