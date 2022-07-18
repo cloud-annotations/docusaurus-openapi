33ad2457fdd3719b1026fb44f9eec0972b028dd2
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-18 16:37:54 +0200

    update license headers

--- packages/docusaurus-plugin-openapi/src/docs/frontMatter.ts
+++ packages/docusaurus-plugin-openapi/src/docs/frontMatter.ts
@@ -5,7 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-// taken from https://github.com/facebook/docusaurus/tree/main/packages/docusaurus-plugin-content-docs
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Taken and adapted from:
+ * https://github.com/facebook/docusaurus/blob/main/packages/docusaurus-plugin-content-docs/src/frontMatter.ts
+ */
 
 import type { DocFrontMatter } from "@docusaurus/plugin-content-docs";
 import {
